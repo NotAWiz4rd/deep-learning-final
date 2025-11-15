294ca1fd413020524304db198746f63374b7d182
notawiz4rd <github@notawiz4rd.com>
2025-11-15 20:11:35 +0100

    add some hyperparameter changes to match original GPT better

--- train_gpt2.py
+++ train_gpt2.py
@@ -9,7 +9,7 @@ from gpt_model import GPT, GPTConfig
 # autodetect device
 device = "cpu"
 if torch.backends.mps.is_available():
-    device = "cpu" # default to cpu on Mac for now, as mps is super slow
+    device = "cpu"  # default to cpu on Mac for now, as mps is super slow
 elif torch.cuda.is_available():
     device = "cuda"
 print("using device:", device)
@@ -23,13 +23,13 @@ elif device == "cuda":
 
 train_loader = DataLoaderLite(B=8, T=1024)
 
-model = GPT(GPTConfig())
+model = GPT(GPTConfig())  # todo potentially set vocab size to 50304 to optimise for nice numbers in CUDA
 model.to(device)
 model = torch.compile(model)
 
 # optimise!
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
-for i in range(4):
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-9)
+for i in range(5):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x = x.to(device)
@@ -41,6 +41,7 @@ for i in range(4):
     else:
         logits, loss = model(x, y)
     loss.backward()
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) # gradient clipping to prevent shocking model updates
     optimizer.step()
 
     if device == "mps":
@@ -50,9 +51,9 @@ for i in range(4):
     elif device == "cpu":
         torch.cpu.synchronize()
     t1 = time.time()
-    dt = (t1 - t0) * 1000  # time difference in ms
-    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step {i}, loss {loss.item():.5f}, time {dt:.2f}ms, {tokens_per_sec:.2f} tokens/sec")
+    dt = (t1 - t0)  # time difference
+    tokens_per_sec = (train_loader.B * train_loader.T) / dt
+    print(f"step {i:4d} | loss: {loss.item():.6f} | time: {dt*1000:.2f}ms | norm: {norm:.4f} | speed: {tokens_per_sec:.2f} tokens/sec")
 
 import sys;
 
