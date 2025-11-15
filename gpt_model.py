1e69774c1b1a0e9a70c0284ec7b82523c4396095
notawiz4rd <github@notawiz4rd.com>
2025-11-15 17:09:44 +0100

    add weight initialisation that matches up with original GPT2 inits

--- gpt_model.py
+++ gpt_model.py
@@ -32,6 +32,9 @@ class GPT(nn.Module):
         # weight sharing scheme
         self.transformer.wte.weight = self.lm_head.weight
 
+        # initialize weights
+        self.apply(self._init_weights)
+
     @classmethod
     def from_pretrained(cls, model_type):
         assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
@@ -77,6 +80,17 @@ class GPT(nn.Module):
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k])
         return model
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'GPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** 0.5
+            torch.nn.init.normal_(module.weight, mean=0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        if isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0, std=0.02)
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
