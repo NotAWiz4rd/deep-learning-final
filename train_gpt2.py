9eca449d7b1df51590a89c7e7915f6e385dd3550
notawiz4rd <github@notawiz4rd.com>
2025-11-15 20:24:55 +0100

    add learning grade decay

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,3 +1,4 @@
+import math
 import time
 
 import torch
@@ -27,9 +28,31 @@ model = GPT(GPTConfig())  # todo potentially set vocab size to 50304 to optimise
 model.to(device)
 model = torch.compile(model)
 
+max_learning_rate = 6e-4
+min_learning_rate = max_learning_rate * 0.1
+warmup_steps = 4
+max_steps = 20
+
+
+def get_learning_rate(step):
+    # 1) linear warmup for warmup_step steps
+    if step < warmup_steps:
+        return max_learning_rate * (step + 1) / warmup_steps
+
+    # 2) if step > max_steps: return min_learning_rate
+    if step > max_steps:
+        return min_learning_rate
+
+    # 3) in between, use cosine decay down to min_learning_rate
+    decay_ratio = (step - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1 + math.cos(math.pi * decay_ratio))  # coeff starts at 1 and goes to 0
+    return min_learning_rate + coeff * (max_learning_rate - min_learning_rate)
+
+
 # optimise!
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-9)
-for i in range(5):
+optimizer = torch.optim.AdamW(model.parameters(), lr=6e-4, betas=(0.9, 0.95), eps=1e-9)
+for step in range(max_steps):
     t0 = time.time()
     x, y = train_loader.next_batch()
     x = x.to(device)
@@ -41,9 +64,15 @@ for i in range(5):
     else:
         logits, loss = model(x, y)
     loss.backward()
-    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) # gradient clipping to prevent shocking model updates
+    # gradient clipping to prevent shocking model updates
+    norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    # determine and set the learning rate for this iteration
+    learning_rate = get_learning_rate(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = learning_rate
     optimizer.step()
 
+    # wait to finish work before measuring time
     if device == "mps":
         torch.mps.synchronize()
     elif device == "cuda":
@@ -53,7 +82,8 @@ for i in range(5):
     t1 = time.time()
     dt = (t1 - t0)  # time difference
     tokens_per_sec = (train_loader.B * train_loader.T) / dt
-    print(f"step {i:4d} | loss: {loss.item():.6f} | time: {dt*1000:.2f}ms | norm: {norm:.4f} | speed: {tokens_per_sec:.2f} tokens/sec")
+    print(
+        f"step {step:4d} | loss: {loss.item():.6f} | time: {dt * 1000:.2f}ms | norm: {norm:.4f} | speed: {tokens_per_sec:.2f} tokens/sec")
 
 import sys;
 
